57851ff18bdde5b191db5bf7e7a53426fa59fba5
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-11 13:27:43 +0530

    five

--- five.c
+++ five.c
@@ -1,21 +1,31 @@
+#include<stdio.h>
+int isselfdivide(int n){
+    int num = n;
+    while(n > 0){
+        int ls = n % 10 ;
+        if(ls == 0){
+            return 0;
+        }
 
-#include <stdio.h>
-
-int main() {
-    int num = 8;
-    int count = 0 ;
-    while (num > 0){
-        if(num % 2 == 0){
-            count++ ;
-            num /=2 ;
+        if (num % ls == 0) {
+            n /= 10;
         }
         else{
-            num -= 1;
-            count++;
+            return 0;
         }
     }
 
-    printf("%d",count);
-
-    return 0;
+    return 1; 
+}
+int main ( ) {
+    int left = 1;
+    int right = 22;
+    for(int i = left ; i <= right ; i++){
+        int result = isselfdivide(i);
+        if(result == 1){
+            printf("%d ",i);
+        }
+        
+    }
+    return 0 ;
 }
